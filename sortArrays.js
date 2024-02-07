a5efd745bca9ca8438bbb75c5e99ced7fc1fcad4
marwan-pixel <marwanhadid6@gmail.com>
2024-02-07 22:33:26 +0700

    Codewars 6 Kyu Sort Arrays (Ignoring Case)

--- sortArrays.js
+++ sortArrays.js
@@ -8,3 +8,8 @@ Sort the given array of strings in alphabetical order, case insensitive. For exa
 ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 */
 
+sortme = function( names ){
+    return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+}
+
+console.log(sortme(["Hello", 'there', "I'm", "fine"]));
