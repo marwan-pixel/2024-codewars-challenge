3f47d75aa2fb13e52787c164765bd89f45ae3728
marwan-pixel <marwanhadid6@gmail.com>
2024-02-14 11:23:03 +0700

    Codewars 7 Kyu Basic JS - Calculating averages

---
+++ calculatingAvg.js
@@ -0,0 +1,20 @@
+/*
+Codewars 7 Kyu Basic JS - Calculating averages
+URL: https://www.codewars.com/kata/529f32794a6db5d32a00071f
+
+Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+
+The test expects you to provide a Calculator object with an average method:
+
+Calculator.average()
+The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+
+It expects Calculator.average(3,4,5) to return 4.
+*/
+
+var Calculator = {
+    average: function (...number) {
+        // Your code here... 
+
+    }
+};
