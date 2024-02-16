1a9d367236298b31278cb012b476343ec16dcb54
marwan-pixel <marwanhadid6@gmail.com>
2024-02-16 20:58:47 +0700

    Codewars 8 Kyu Simple multiplication

---
+++ simpleMultiplication.js
@@ -0,0 +1,11 @@
+/*
+Codewars 8 Kyu Simple multiplication
+URL: https://www.codewars.com/kata/583710ccaa6717322c000105
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+const simpleMultiplication = (number) =>  number % 2 === 0 ? number * 8 : number * 9;
+
+
+console.log(simpleMultiplication(4));
