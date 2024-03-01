b326415acfc2f03af5ade73301c44c111c3f659d
marwan-pixel <marwanhadid6@gmail.com>
2024-03-01 21:55:21 +0700

    Codewars Kyu 8 Sum Mixed Array

---
+++ sumMixedArray.js
@@ -0,0 +1,14 @@
+/*
+Codewars Kyu 8 Sum Mixed Array
+URL:https://www.codewars.com/kata/57eaeb9578748ff92a000009
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(x){
+    return x.reduce((accu, curr) => accu + +curr, 0)
+}
+
+console.log(sumMix([9, 3, '7', '3']));
