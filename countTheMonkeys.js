9719fe3509cf6f8700083f41af0cce499a9d296f
marwan-pixel <marwanhadid6@gmail.com>
2024-04-09 12:40:34 +0700

    Codewars 8 Kyu Count the Monkeys!

--- countTheMonkeys.js
+++ countTheMonkeys.js
@@ -12,14 +12,6 @@ For example(Input --> Output):
  1 --> [1]
 */
 
-function monkeyCount(n) {
-  // your code here
-  // const result = [];
-  // for (let i = 1; i <= n; i++) {
-  //  result.push(i);
-  // }
-  // return result;
-  return Array.from({ length: n }, (_, i) => i + 1);
-}
+const monkeyCount = (n) => Array.from({ length: n }, (_, i) => i + 1);
 
 console.log(monkeyCount(10));
