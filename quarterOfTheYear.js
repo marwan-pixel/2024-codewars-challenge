42a14b1bfda66d4d66371c10ba3e8022aab65c51
marwan-pixel <marwanhadid6@gmail.com>
2024-04-09 12:40:42 +0700

    Codewars 8 Kyu Quarter of the year

--- quarterOfTheYear.js
+++ quarterOfTheYear.js
@@ -12,9 +12,6 @@ Constraint:
 
 */
 
-const quarterOf = (month) => {
-  // Your code here
-  return Math.ceil(month / 3);
-};
+const quarterOf = (month) => Math.ceil(month / 3);
 
-console.log(quarterOf(11));
+console.log(quarterOf(1));
